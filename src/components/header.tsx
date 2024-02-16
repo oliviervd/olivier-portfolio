def3b64aec9f5018087876a1003167bd5c21d3b3
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-02-16 05:18:58 +0100

    init styling + add header

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return(
+        <header>
+            <h1 className={"typo_header"}>Olivier Van D'huynslager</h1>
+            <div/>
+            <div className={"right"}>
+                <h1 className={"right typo_header"}><a>curatorial</a></h1>
+                <h1 className={"right typo_header"}><a>music</a></h1>
+                <h1 className={"right typo_header"}><a>about</a></h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
--- src/home.tsx
+++ src/home.tsx
@@ -1,10 +1,11 @@
 import { render } from 'preact';
 import './style.css';
+import Header from "./components/header";
 
 export function App() {
 	return (
 		<div>
-
+			<Header/>
 		</div>
 	);
 }
