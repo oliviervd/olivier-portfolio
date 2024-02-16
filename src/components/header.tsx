ed2fcc90a897b07a19b1c5548633c7c95f4345e0
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-02-16 05:41:43 +0100

    init styling + add short bio

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,12 @@
 const Header = () => {
     return(
         <header>
-            <h1 className={"typo_header"}>Olivier Van D'huynslager</h1>
+            <h1 className={"typo_header Anthony"}>Olivier Van D'huynslager</h1>
             <div/>
             <div className={"right"}>
                 <h1 className={"right typo_header"}><a>curatorial</a></h1>
                 <h1 className={"right typo_header"}><a>music</a></h1>
+                <h1 className={"right typo_header"}><a>code</a></h1>
                 <h1 className={"right typo_header"}><a>about</a></h1>
             </div>
         </header>
--- src/home.tsx
+++ src/home.tsx
@@ -6,6 +6,9 @@ export function App() {
 	return (
 		<div>
 			<Header/>
+			<div class={"box__small"}>
+				<p>Olivier Van D'huynslager is a Belgian curator, musician and coder based in Ghent, Belgium. Currently active as curator of digital culture and design at Design Museum Gent. Aside, he is a freelance musician and coder, mainly working on web-design.</p>
+			</div>
 		</div>
 	);
 }
