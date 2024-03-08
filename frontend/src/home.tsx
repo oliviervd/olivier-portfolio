fa5b52d1bd0994e62ae6adc4f3affd0a1402f12b
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-08 06:42:46 +0100

    update frontend

--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -9,7 +9,7 @@ export function App() {
 			<Header/>
 			<div class={"box__small"}>
 				<p>IS CURRENTLY MIGRATING FROM ONE CLOUD TO ANOTHER.</p>
-				<p>Olivier Van D'huynslager is a Belgian curator, musician and coder based in Ghent, Belgium. Currently active as curator of digital culture and design at Design Museum Gent. Aside, he is a freelance musician and coder, mainly working on web-design.</p>
+				<p>Olivier Van D'huynslager is a Belgian curator and strategist based in Ghent, Belgium. Currently active as curator of digital culture and design at Design Museum Gent. Aside, he is a freelance musician and coder, mainly working on web-design.</p>
 			</div>
 			<Footer/>
 		</div>
