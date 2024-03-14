a3dc74368327b3ef1b714c1855473e2fbd32c48b
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-14 19:06:36 +0100

    update style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,13 +2,10 @@ const Header = () => {
     return(
         <header>
             <h1 className={"typo_header"}>Olivier Van D'huynslager</h1>
-            <div/>{/*
+            <div/>
             <div className={"right"}>
-                <h1 className={"right typo_header"}><a>curatorial</a></h1>
                 <h1 className={"right typo_header"}><a>music</a></h1>
-                <h1 className={"right typo_header"}><a>code</a></h1>
-                <h1 className={"right typo_header"}><a>about</a></h1>
-            </div>*/}
+            </div>
         </header>
     )
 }
