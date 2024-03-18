13ac5b608e40a9f23b4d2b99fb36809430c25e7b
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-18 21:08:58 +0100

    update home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,9 @@ const Header = () => {
             <div/>
             <div className={"right"}>
                 <h1 className={"right typo_header"}><a>music</a></h1>
+                <h1 className={"right typo_header"}><a>code</a></h1>
+                <h1 className={"right typo_header"}><a>cv</a></h1>
+                <h1 className={"right typo_header"}><a>curatorial</a></h1>
             </div>
         </header>
     )
