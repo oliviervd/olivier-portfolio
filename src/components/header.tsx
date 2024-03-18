5b6fe9b78903fa860086580fdda971c43ba24200
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-18 21:10:43 +0100

    update styling home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,10 +4,10 @@ const Header = () => {
             <h1 className={"typo_header"}>Olivier Van D'huynslager</h1>
             <div/>
             <div className={"right"}>
-                <h1 className={"right typo_header"}><a>music</a></h1>
-                <h1 className={"right typo_header"}><a>code</a></h1>
-                <h1 className={"right typo_header"}><a>cv</a></h1>
-                <h1 className={"right typo_header"}><a>curatorial</a></h1>
+                <h1 className={"right typo_header inactive"}><a>music</a></h1>
+                <h1 className={"right typo_header inactive"}><a>code</a></h1>
+                <h1 className={"right typo_header inactive"}><a>cv</a></h1>
+                <h1 className={"right typo_header inactive"}><a>curatorial</a></h1>
             </div>
         </header>
     )
