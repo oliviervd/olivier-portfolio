9d2e7171332444373c3181d8383030e9d597ce44
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-24 12:35:26 +0100

    update styling links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className={"fonts"}>
                 <p>fonts: </p>
                 <div>
-                    <p>Copyright (c) Benjamin Gomez, with Reserved Font Name Basalte.</p>
+                    <p>Copyright (c) <span><a href={"www.depli-ds.com"}>Benjamin Gomez</a></span>, with Reserved Font Name Basalte.</p>
                 </div>
             </div>
         </footer>
