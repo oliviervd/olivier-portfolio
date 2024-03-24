f7a7f0075fec78e309e7bbe090e0c895d1fd7876
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-24 15:22:49 +0100

    add init button for mobile view

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,18 @@
+import Button from "../sketches/button";
+
 const Header = (props) => {
     return(
         <header>
             <h1 className={"typo_header"}>Olivier Van D'huynslager</h1>
             <div/>
-            <div className={"right"}>
-                <h1 className={"right typo_header inactive"}><a>music</a></h1>
-                <h1 className={"right typo_header inactive"}><a>code</a></h1>
-                <h1 className={"right typo_header"} onClick={()=>props.setShowResume(!props.showResume)}><a>cv</a></h1>
-                <h1 className={"right typo_header inactive"}><a>curatorial</a></h1>
+            <div className={"header__nav"}>
+                <h1 className={"header__nav typo_header inactive"}><a>music</a></h1>
+                <h1 className={"header__nav typo_header inactive"}><a>code</a></h1>
+                <h1 className={"header__nav typo_header"} onClick={()=>props.setShowResume(!props.showResume)}><a>cv</a></h1>
+                <h1 className={"header__nav typo_header inactive"}><a>curatorial</a></h1>
+            </div>
+            <div className={"header__nav-button"}>
+                <Button />
             </div>
         </header>
     )
