11ba84e06f3c483b00d1c2bbc080504d10fe62c7
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-30 15:55:57 +0100

    add "is reading"

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,8 +7,9 @@ const Header = (props) => {
     return (
         <header>
             <div className={"header_logo"}>
-                <h1 className={"little-weave"}> ⩨</h1>
+                <h1 className={"little-weave"}>⩨</h1>
                 <h1 className={"typo_header"}>OVND</h1>
+                <h1 className={"typo_header reading"}> – is reading: <span><a href={"https://www.legacyrussell.com/GLITCHFEMINISM"} target="_blank">Glitch Feminism</a></span></h1>
             </div>
             <div className={!props.menuOpen ? "header__nav off-screen" : "header__nav on-screen"}>
                 <h1 className={"typo_header"} onClick={() => props.toggleAbout()}><a>about</a></h1>
