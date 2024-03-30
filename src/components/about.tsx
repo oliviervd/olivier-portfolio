0f2011acd809e4faf14a87086a1b89d86743e0b7
oliviervd <oliviervandhuynslager@gmail.com>
2024-03-30 17:51:49 +0100

    clean code

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,27 +1,11 @@
-import {useState, useEffect} from "preact/hooks"
-import {fetchPayload} from "../utils/fetchPayload";
-import serialize from "../utils/serialize";
 
 interface AboutStates {
     about: "array"
 }
 
-const About = () => {
-
-    const [about, setAbout] = useState([])
-
-    useEffect(() => {
-        fetchPayload("https://p01--admin--cvvgvqwlxhx2.code.run", "about", 10).then((data)=>{
-            // loop over and fetch type "about
-            console.log(data)
-            const _unserializedText = data.docs[0]["bio"]
-            const _serializedText = serialize(_unserializedText)
-            setAbout(_serializedText)
-        })
-    }, []);
-
+const About = (props) => {
     return(
-        about.map((a)=>{
+        props.about.map((a)=>{
             return(
                 <div>
                     <div className={'about'}>{a}</div>
