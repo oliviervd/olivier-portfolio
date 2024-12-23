4c328619c8897179853301fa5b5ade308c570531
oliviervd <oliviervandhuynslager@gmail.com>
2024-12-23 20:17:33 +0100

    setup sitemap to improve SEO

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import { LocationProvider, Route , Router } from 'preact-iso';
 import { h, render } from 'preact';
 
 import {Home} from "./pages/home";
+import {Project} from "./pages/project";
 
 const App= () => (
     <LocationProvider>
             <Router>
                 <Route path="/" component={Home} />
-                <Route default component={Home} />
+                <Route path="/project/:id?" component={Project} />
             </Router>
     </LocationProvider>
 )
