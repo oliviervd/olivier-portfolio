8d170f6605256f0beec9da7512a9ac71ca57a09d
oliviervd <oliviervandhuynslager@gmail.com>
2024-12-24 11:44:09 +0100

    set base for vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	base: '/',
 	build: {
 		rollupOptions: {
 			input: 'src/index.tsx'
