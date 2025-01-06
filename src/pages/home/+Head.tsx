52b5cb039f56a4d31d5a3122aa1851edb2adecba
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2025-01-06 21:59:12 +0100

    "Refactor head metadata into a standalone component
    Extracted page metadata logic from the homepage component to a new `Head` component for improved modularity and maintainability. This reduces duplication and centralizes metadata management."

---
+++ src/pages/home/+Head.tsx
@@ -0,0 +1,21 @@
+export function Head() {
+    return (
+        <>
+            <title>Olivier Van D'huynslager - Homepage</title>
+            <meta name="description" content="Discover Olivier Van D'huynslager's homepage and personal projects."/>
+            <meta name="author" content="Olivier Van D'huynslager"/>
+            <meta name="keywords" content="Olivier Van D'huynslager, Olivier, Van D'huynslager, books, library, bookshelf, p5, sketch, curatorial, museum, design museum gent"/>
+            <meta property="og:url" content="https://oliviervandhuynslager.net"/>
+            <meta property="og:type" content="website"/>
+            <meta name="og:title" content="Olivier Van D'huynslager - Homepage"/>
+            <meta name="og:description" content="Discover Olivier Van D'huynslager's homepage and personal projects."/>
+            <script type="application/ld+json">
+                {`
+                     {
+                       "@context": "https://schema.org",
+                       "@type": "WebPage",
+                `}
+            </script>
+        </>
+    )
+}
--- src/pages/home/+Page.tsx
+++ src/pages/home/+Page.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { Home } from '../../components/main/home';
-import {Head} from "vike-react/Head"
 
 export const Page = () => {
     return (
         <>
-            <Head>
-                <title>Olivier Van D'huynslager - Homepage</title>
-                <meta name="description" content="Homepage of Olivier Van D'huynslager - website"/>
-                <meta property="og:title" content="Olivier Van D'huynslager - Homepage"/>
-                <meta property="og:description" content="Homepage of Olivier Van D'huynslager - website"/>
-            </Head>
             <Home/>
         </>
 
